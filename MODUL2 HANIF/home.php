3170127ecf43ea39bba645d7ce5ed9b0c4637bcf
Akbar Affaruk Khuzaimi Ahmadani <akbaraffaruk13@gmail.com>
2021-10-29 15:32:15 +0700

    easyLah

--- MODUL2 HANIF/home.php
+++ MODUL2 HANIF/home.php
@@ -1,113 +1,113 @@
-<!DOCTYPE html>
+    <!DOCTYPE html>
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>ESD Venue</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>ESD Venue</title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
 
-    <section id="Nav">
-        <ul class="nav bg-dark justify-content-center p-2">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="booking.php">Booking</a>
-            </li>
-        </ul>
-    </section>
-    <section id="Home">
-        <div class="container justify-content-center">
-            <center><h4 class="m-4">WELCOME TO ESD VENUE RESERVATION</h4></center>
-            <div class="bg-dark p-2"><div class="subjudul">Find your best deal for your event, here!</div></div>
-            
-            <form action="booking.php" method="POST">
-            <div class="row align-items-center pt-3">
-                <div class="col">
-                    <div class="card" style="width: auto;">
-                        <img src="img-1.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Nusantara Hall</h5>
-                            <p class="card-text">
-                                $2000 / hour <br>
-                                5000 Capacity
-                            </p>
-                        </div>
-                        <ul class="list-group list-group-flush align-center fw-bold">
-                            <li class="list-group-item on">Free Parking</li>
-                            <li class="list-group-item on">Full AC</li>
-                            <li class="list-group-item on">Cleaning Service</li>
-                            <li class="list-group-item on">Covid-19 Helath Protocol</li>
-                        </ul>
-                        <div class="card-body-button align-center">
-                            <button type="submit" name="nama_gedung" value="Nusantara Hall" class="btn btn-primary">Book Now</button>
-                        </div>
-                    </div>
+        <section id="Nav">
+            <ul class="nav bg-dark justify-content-center p-2">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+            </ul>
+        </section>
+        <section id="Home">
+            <div class="container justify-content-center">
+                <center><h4 class="m-4">WELCOME TO ESD VENUE RESERVATION</h4></center>
+                <div class="bg-dark p-2"><div class="subjudul">Find your best deal for your event, here!</div></div>
                 
-                </div>
-                <div class="col">
-
-                    <div class="card" style="width: auto;">
-                        <img src="img-2.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Garuda Hall</h5>
-                            <p class="card-text">
-                                $1000 / hour <br>
-                                2000 Capacity
-                            </p>
+                <form action="booking.php" method="POST">
+                <div class="row align-items-center pt-3">
+                    <div class="col">
+                        <div class="card" style="width: auto;">
+                            <img src="img-1.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Nusantara Hall</h5>
+                                <p class="card-text">
+                                    $2000 / hour <br>
+                                    5000 Capacity
+                                </p>
+                            </div>
+                            <ul class="list-group list-group-flush align-center fw-bold">
+                                <li class="list-group-item on">Free Parking</li>
+                                <li class="list-group-item on">Full AC</li>
+                                <li class="list-group-item on">Cleaning Service</li>
+                                <li class="list-group-item on">Covid-19 Helath Protocol</li>
+                            </ul>
+                            <div class="card-body-button align-center">
+                                <button type="submit" name="nama_gedung" value="Nusantara Hall" class="btn btn-primary">Book Now</button>
+                            </div>
                         </div>
-                        <ul class="list-group list-group-flush align-center fw-bold">
-                            <li class="list-group-item on">Free Parking</li>
-                            <li class="list-group-item on">Full AC</li>
-                            <li class="list-group-item off">No Cleaning Service</li>
-                            <li class="list-group-item on">Covid-19 Helath Protocol</li>
-                        </ul>
-                        <div class="card-body-button align-center">
-                            <button type="submit" class="btn btn-primary" name="nama_gedung" value="Garuda Hall">Book Now</button>
-                        </div>
+                    
                     </div>
+                    <div class="col">
 
-                </div>
-                <div class="col">
+                        <div class="card" style="width: auto;">
+                            <img src="img-2.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Garuda Hall</h5>
+                                <p class="card-text">
+                                    $1000 / hour <br>
+                                    2000 Capacity
+                                </p>
+                            </div>
+                            <ul class="list-group list-group-flush align-center fw-bold">
+                                <li class="list-group-item on">Free Parking</li>
+                                <li class="list-group-item on">Full AC</li>
+                                <li class="list-group-item off">No Cleaning Service</li>
+                                <li class="list-group-item on">Covid-19 Helath Protocol</li>
+                            </ul>
+                            <div class="card-body-button align-center">
+                                <button type="submit" class="btn btn-primary" name="nama_gedung" value="Garuda Hall">Book Now</button>
+                            </div>
+                        </div>
 
-                    <div class="card" style="width: auto;">
-                        <img src="img-3.jpg" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title">Gedung Serba Guna</h5>
-                            <p class="card-text">
-                                $500 / hour <br>
-                                500 Capacity
-                            </p>
-                        </div>
-                        <ul class="list-group list-group-flush align-center fw-bold">
-                            <li class="list-group-item off">No Free Parking</li>
-                            <li class="list-group-item off">No Full AC</li>
-                            <li class="list-group-item off">No Cleaning Service</li>
-                            <li class="list-group-item on">Covid-19 Helath Protocol</li>
-                        </ul>
-                        <div class="card-body-button align-center">
-                            <button type="submit" class="btn btn-primary" name="nama_gedung" value="Gedung Serba Guna">Book Now</button>
-                        </div>
                     </div>
+                    <div class="col">
 
+                        <div class="card" style="width: auto;">
+                            <img src="img-3.jpg" class="card-img-top" alt="...">
+                            <div class="card-body">
+                                <h5 class="card-title">Gedung Serba Guna</h5>
+                                <p class="card-text">
+                                    $500 / hour <br>
+                                    500 Capacity
+                                </p>
+                            </div>
+                            <ul class="list-group list-group-flush align-center fw-bold">
+                                <li class="list-group-item off">No Free Parking</li>
+                                <li class="list-group-item off">No Full AC</li>
+                                <li class="list-group-item off">No Cleaning Service</li>
+                                <li class="list-group-item on">Covid-19 Helath Protocol</li>
+                            </ul>
+                            <div class="card-body-button align-center">
+                                <button type="submit" class="btn btn-primary" name="nama_gedung" value="Gedung Serba Guna">Book Now</button>
+                            </div>
+                        </div>
+
+                    </div>
                 </div>
+                </form>
             </div>
-            </form>
+        </section>
+        
+        <section>
+        <div class="footer py-3 bg-light mt-10">
+            <div class="container">
+                created by Hanif_1202194185
+            </div>
         </div>
-    </section>
-    
-    <section>
-    <div class="footer py-3 bg-light mt-10">
-        <div class="container">
-            created by Hanif_1202194185
-        </div>
-    </div>
-    </section>
+        </section>
 
-    </body>
-</html>
+        </body>
+    </html>
--- MODUL2 HANIF/mybooking.php
+++ MODUL2 HANIF/mybooking.php
@@ -1,123 +1,123 @@
-<?php
-    $name = isset($_POST['name']) ? $_POST['name'] : '' ;
-    $event_date = isset($_POST['event_date']) ? $_POST['event_date'] : '' ;
-    $duration = isset($_POST['duration']) ? $_POST['duration'] : '' ;
-    $start_time = isset($_POST['start_time']) ? $_POST['start_time'] : '' ;
-    $checkin = date('d-m-yy', strtotime($event_date)) ." ". date('H:i:s', strtotime($start_time));
-    $date = date('H:i:s', strtotime($start_time.' + '.$duration.' hours'));
-    $checkout = date('d-m-yy', strtotime($event_date)) ." ". $date;
-    $nama_gedung = isset($_POST['nama_gedung']) ? $_POST['nama_gedung'] : '' ;
-    $phone_number = isset($_POST['phone_number']) ? $_POST['phone_number'] : '' ;
-    $catering = isset($_POST['catering']) ? $_POST['catering'] : '' ;
-    $decoration = isset($_POST['decoration']) ? $_POST['decoration'] : '' ;
-    $sound_system = isset($_POST['sound_system']) ? $_POST['sound_system'] : '' ;
-    
-    if($nama_gedung == "Nusantara Hall"){
-        $total_price = $duration*2000;
-    }else if($nama_gedung == "Garuda Hall"){
-        $total_price = $duration*1000;
-    }else{
-        $total_price = $duration*500;
-    }
+    <?php
+        $name = isset($_POST['name']) ? $_POST['name'] : '' ;
+        $event_date = isset($_POST['event_date']) ? $_POST['event_date'] : '' ;
+        $duration = isset($_POST['duration']) ? $_POST['duration'] : '' ;
+        $start_time = isset($_POST['start_time']) ? $_POST['start_time'] : '' ;
+        $checkin = date('d-m-yy', strtotime($event_date)) ." ". date('H:i:s', strtotime($start_time));
+        $date = date('H:i:s', strtotime($start_time.' + '.$duration.' hours'));
+        $checkout = date('d-m-yy', strtotime($event_date)) ." ". $date;
+        $nama_gedung = isset($_POST['nama_gedung']) ? $_POST['nama_gedung'] : '' ;
+        $phone_number = isset($_POST['phone_number']) ? $_POST['phone_number'] : '' ;
+        $catering = isset($_POST['catering']) ? $_POST['catering'] : '' ;
+        $decoration = isset($_POST['decoration']) ? $_POST['decoration'] : '' ;
+        $sound_system = isset($_POST['sound_system']) ? $_POST['sound_system'] : '' ;
+        
+        if($nama_gedung == "Nusantara Hall"){
+            $total_price = $duration*2000;
+        }else if($nama_gedung == "Garuda Hall"){
+            $total_price = $duration*1000;
+        }else{
+            $total_price = $duration*500;
+        }
 
-    if (isset($_POST['add_service'])) {
-        $add_service = $_POST['add_service'];
-    }
-    else {
-        $add_service = '';
-    }
+        if (isset($_POST['add_service'])) {
+            $add_service = $_POST['add_service'];
+        }
+        else {
+            $add_service = '';
+        }
 
-?>
+    ?>
 
-<!DOCTYPE html>
+    <!DOCTYPE html>
 
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>ESD Venue</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="style.css">
-    </head>
-    <body>
-    <section id="Nav">
-        <ul class="nav bg-dark justify-content-center p-2">
-            <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="home.php">Home</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="booking.php">Booking</a>
-            </li>
-        </ul>
-    </section>
-    <section id="Home">
+    <html>
+        <head>
+            <meta charset="utf-8">
+            <meta http-equiv="X-UA-Compatible" content="IE=edge">
+            <title>ESD Venue</title>
+            <meta name="description" content="">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link rel="stylesheet" href="style.css">
+        </head>
+        <body>
+        <section id="Nav">
+            <ul class="nav bg-dark justify-content-center p-2">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="home.php">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="booking.php">Booking</a>
+                </li>
+            </ul>
+        </section>
+        <section id="Home">
 
-        <div class="container text-center mt-5">
-        <h4>
-            Thank you <?= $name ?> for Reserving
-        </h4>
-        Please double check your reservation details
+            <div class="container text-center mt-5">
+            <h4>
+                Thank you <?= $name ?> for Reserving
+            </h4>
+            Please double check your reservation details
 
-        <table class="table table-striped table-hover mt-5">
-            <thead>
-                <tr>
-                <th scope="col">Booking Number</th>
-                <th scope="col">Name</th>
-                <th scope="col">Check-in</th>
-                <th scope="col">Check-out</th>
-                <th scope="col">Building Type</th>
-                <th scope="col">Phone Number</th>
-                <th scope="col">Service</th>
-                <th scope="col">Total Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <th scope="row"><?= rand(1,1000000)?></th>
-                    <td><?= $name ?></td>
-                    <td><?= $checkin ?></td>
-                    <td><?= $checkout ?></td>
-                    <td><?= $nama_gedung ?></td>
-                    <td><?= $phone_number ?></td>
-                    <td style="text-align: left;"> <ul>
-                        <?php
-                        if (!empty($add_service)) {
-                            echo '<ul>';
-                            foreach ($add_service as $service) {
-                                if($service == "Catering") {
-                                    $total_price += 700;
-                                    echo '<li>' . $service . '</li>';
+            <table class="table table-striped table-hover mt-5">
+                <thead>
+                    <tr>
+                    <th scope="col">Booking Number</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Check-in</th>
+                    <th scope="col">Check-out</th>
+                    <th scope="col">Building Type</th>
+                    <th scope="col">Phone Number</th>
+                    <th scope="col">Service</th>
+                    <th scope="col">Total Price</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row"><? rand(1,1000000)?></th>
+                        <td><? $name ?></td>
+                        <td><? $checkin ?></td>
+                        <td><? $checkout ?></td>
+                        <td><? $nama_gedung ?></td>
+                        <td><? $phone_number ?></td>
+                        <td style="text-align: left;"> <ul>
+                            <?php
+                            if (empty($add_service)) {
+                                echo '<ul>';
+                                foreach ($add_service as $service) {
+                                    if($service == "Catering") {
+                                        $total_price += 700;
+                                        echo '<li>' . $service . '</li>';
+                                    }
+                                    elseif($service == "Decoration") {
+                                        $total_price += 450;
+                                        echo '<li>' . $service . '</li>';
+                                    }
+                                    elseif($service == "Sound System") {
+                                        $total_price += 250;
+                                        echo '<li>' . $service . '</li>';
+                                    }
+                                    
                                 }
-                                elseif($service == "Decoration") {
-                                    $total_price += 450;
-                                    echo '<li>' . $service . '</li>';
-                                }
-                                elseif($service == "Sound System") {
-                                    $total_price += 250;
-                                    echo '<li>' . $service . '</li>';
-                                }
-                                
+                                echo '</ul>';
+                            } else {
+                                echo 'no service';
                             }
-                            echo '</ul>';
-                        } else {
-                            echo 'no service';
-                        }
-                        ?>
-                    </ul></td>
-                    <td><?= '$' . $total_price ?></td>
-                </tr>
-            </tbody>
-        </table>
+                            ?>
+                        </ul></td>
+                        <td><? '$' . $total_price ?></td>
+                    </tr>
+                </tbody>
+            </table>
+            </div>
+        </section>
+        <section class="mt-5 mt-5" style="padding-top: 430px;">
+        <div class="footer py-3 bg-light">
+            <div class="container">
+                created by Hanif_1202194185
+            </div>
         </div>
-    </section>
-    <section class="mt-5 mt-5" style="padding-top: 430px;">
-    <div class="footer py-3 bg-light">
-        <div class="container">
-            created by Hanif_1202194185
-        </div>
-    </div>
-    </section>
-    
-    </body>
-</html>
+        </section>
+        
+        </body>
+    </html>
